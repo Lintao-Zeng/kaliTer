08530d1e0d6638d928888614a5d7a18d9b8b5ae5
zenglintao <2534324260@qq.com>
2021-06-17 19:00:07 +0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -6,7 +6,7 @@ f2 = open("test.session.sql","a")
 line = f.readline()
 while line:
     arr = line.replace("\n","").split('@@')
-    f2.write("insert into test values ('"+arr[0]+"','"+arr[1]+"','"+arr[2]+"');\r\n")
+    f2.write("insert into test values ('"+arr[0]+"','"+arr[1]+"','"+arr[2]+"');\n")
     line = f.readline()
 print("写入完成")
 f.close()
